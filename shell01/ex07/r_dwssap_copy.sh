b327a925795ca3b8d761647e8f53cbf8b8bfe60a
Mickael Marcotte <mmarcott@c1r1p1.42quebec.com>
2022-09-14 09:49:40 -0400

    update morning mer

--- shell01/ex07/r_dwssap_copy.sh
+++ shell01/ex07/r_dwssap_copy.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/^[[:blank:]]*#/d;s/#.*//' | sed -e "s/:\*:.*//" | rev | sed -n "n;p" | sort -r | tr ' ' ', ' | tr -d '\n'
+cat /etc/passwd | grep -v '#' | sed -e "s/:\*:.*//" | rev | sed -n "n;p" | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' | sort -r | tr '\n' ' ' | tr -d '\n'
