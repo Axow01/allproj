b49a8618f50854d77f99a2cc679a7d3172016810
Mickael Marcotte <mmarcott@c1r1p4.42quebec.com>
2022-09-16 19:20:13 -0400

    ft_print_combn

--- c01/ex00/ft_ft.c
+++ c01/ex00/ft_ft.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarcott <mickael.marcotte2004@gmail.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/16 11:42:29 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/09/16 13:18:48 by mmarcott         ###   ########.fr       */
+/*   Created: 2022/09/16 12:12:30 by mmarcott          #+#    #+#             */
+/*   Updated: 2022/09/16 12:20:02 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
 
-void ft_print_combn(int n)
+void	ft_ft(int *nbr)
 {
-	
-}
-
-int	main(void)
-{
-	ft_print_combn(2);
+	*nbr = 42;
 }
