18d0566ab93235e513b217f9f25673e1f5869c34
Mickael Marcotte <mmarcott@c1r1p2.42quebec.com>
2022-09-19 12:38:55 -0400

    final changes to c01

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,31 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarcott <mickael.marcotte2004@gmail.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/18 15:18:24 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/09/19 12:23:18 by mmarcott         ###   ########.fr       */
+/*   Created: 2022/09/19 11:56:53 by mmarcott          #+#    #+#             */
+/*   Updated: 2022/09/19 12:33:52 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdio.h> // Remove after testing.
 
-void	ft_rev_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int *tab, int size)
 {
-	int	*swap;
-	int	*swap2;
+	int	index;
+	int	index2;
 	int	buffer;
+	int	*slot1;
+	int	*slot2;
 
-	swap = tab;
-	swap2 = tab + (size - 1);
-	while (swap < swap2)
+	index = 1;
+	index2 = 1;
+	while (index2++ <= size)
 	{
-		buffer = *swap;
-		*swap = *swap2;
-		*swap2 = buffer;
-		swap++;
-		swap2--;
+		slot1 = tab;
+		slot2 = tab + 1;
+		while (index++ < size)
+		{
+			if (*slot1 > *slot2)
+			{
+				buffer = *slot1;
+				*slot1 = *slot2;
+				*slot2 = buffer;
+			}
+			slot1++;
+			slot2++;
+		}
+		index = 1;
 	}
 }
