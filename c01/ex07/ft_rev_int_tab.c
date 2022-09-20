8c4d9aba49a33e75875d2bd5f60aae4fef14beb5
Mickael Marcotte <mmarcott@c1r2p1.42quebec.com>
2022-09-20 13:48:49 -0400

    c

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -6,24 +6,22 @@
 /*   By: mmarcott <mickael.marcotte2004@gmail.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 15:18:24 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/09/19 22:58:39 by mmarcott         ###   ########.fr       */
+/*   Updated: 2022/09/20 12:25:38 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	*swap;
-	int	*swap2;
 	int	buffer;
+	int i;
 
-	swap = tab;
-	swap2 = tab + (size - 1);
-	while (swap < swap2)
+	i = 0;
+	while (i < size)
 	{
-		buffer = *swap;
-		*swap = *swap2;
-		*swap2 = buffer;
-		swap++;
-		swap2--;
+		buffer = tab[i];
+		tab[i] = tab[size - 1];
+		tab[size - 1] = buffer;
+		i++;
+		size--;
 	}
 }
