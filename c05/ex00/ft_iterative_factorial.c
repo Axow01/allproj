758ebb1863c460590d5b4e3e6de7038cf8fd7ec5
Mickael Marcotte <mmarcott@c1r5p8.42quebec.com>
2022-09-27 19:44:16 -0400

    sup

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,11 +6,21 @@
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 19:21:35 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/09/26 19:23:02 by mmarcott         ###   ########.fr       */
+/*   Updated: 2022/09/27 19:43:49 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
+	int	index;
 
+	if (nb == 0)
+		return (0);
+	index = nb - 1;
+	while (index != 1)
+	{
+		nb = nb * index;
+		index--;
+	}
+	return (nb);
 }
