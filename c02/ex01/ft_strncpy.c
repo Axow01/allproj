7d8cc248754a07e0db0da6770b7834d4bcc47aa7
Mickael Marcotte <mmarcott@c1r8p11.42quebec.com>
2022-10-01 17:09:46 -0400

    jai faim calice

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,38 +6,21 @@
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 16:12:12 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/09/28 14:33:59 by mmarcott         ###   ########.fr       */
+/*   Updated: 2022/10/01 17:07:32 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
-{
-	int	index;
-
-	index = 0;
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	char				*destb;
-	unsigned int		i;
+	unsigned int	i;
 
-	destb = dest;
 	i = 0;
-	while (*src != '\0' && i < n)
+	while (src[i] && i < n)
 	{
-		*destb = src[i];
+		dest[i] = src[i];
 		i++;
-		destb++;
 	}
 	while (i < n)
-	{
-		dest[i] = 0;
-		i++;
-	}
-	*destb = '\0';
+		dest[i++] = '\0';
 	return (dest);
 }
