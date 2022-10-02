d820ca4ef503e31324d151c3616c23b129c000a1
Mickael Marcotte <mmarcott@c1r6p8.42quebec.com>
2022-10-01 23:55:43 -0400

    basic bitch shit¡

--- c05/ex03/main.c
+++ c05/ex03/main.c
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 22:48:24 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/10/01 19:43:46 by mmarcott         ###   ########.fr       */
+/*   Updated: 2022/10/01 23:47:27 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	ft_recursive_power(int nb, int power);
 
 int	main(void)
 {
-	printf("%d\n", ft_recursive_power(2, 3));
+	printf("%d\n", ft_recursive_power(200, 3));
 	return (0);
 }
