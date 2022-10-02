08f333d3b854733509a91c35682c1b8c9383aae8
TheMickQc <mickael.marcotte2004@gmail.com>
2022-10-02 12:43:43 -0400

    bonjour

--- c07/ex02/main.c
+++ c07/ex02/main.c
@@ -5,6 +5,16 @@ int	ft_ultimate_range(int **range, int min, int max);
 
 int	main(void)
 {
-	
+	int	*range;
+	int min = -4;
+	int max = 20;
+	int i;
+	int a = ft_ultimate_range(&range, min, max);
+	printf("%d\n", a);
+	for (i = 0; i < (max - min); i++)
+	{
+		printf("%d, ", range[i]);
+	}
+	printf("\n");
 	return (0);
 }
