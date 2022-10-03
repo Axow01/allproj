04dfe9d65fdee86570d0c2ef255cb8ced68932f4
Mickael Marcotte <mmarcott@c1r5p1.42quebec.com>
2022-10-03 16:14:59 -0400

    calice de criss

--- c05/ex02/ft_iterative_power.c
+++ c05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 22:02:36 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/10/02 22:54:45 by mmarcott         ###   ########.fr       */
+/*   Updated: 2022/10/03 16:10:41 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_iterative_power(int nb, int power)
 
 	i = 1;
 	defnb = nb;
-	if ((power == 0 || nb == 0))
+	if ((power == 0 && nb <= 0))
 		return (1);
 	if (power <= 0)
 		return (0);
--- c05/ex02/main.c
+++ c05/ex02/main.c
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 20:42:34 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/10/02 22:58:52 by mmarcott         ###   ########.fr       */
+/*   Updated: 2022/10/03 16:10:14 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@ int	ft_iterative_power(int nb, int power);
 
 int	main(void)
 {
-	printf("%d\n", ft_iterative_power(138, 0));
+	printf("%d\n", ft_iterative_power(2513, -821));
+	printf("%d\n", ft_iterative_power(0, 0));
+	printf("%d\n", ft_iterative_power(-3531, 0));
+	printf("%d\n", ft_iterative_power(4010, 1));
+	printf("%d\n", ft_iterative_power(0, 2));
+	printf("%d\n", ft_iterative_power(1, 3));
+	printf("%d\n", ft_iterative_power(2, 4));
+	printf("%d\n", ft_iterative_power(3, 5));
+	printf("%d\n", ft_iterative_power(-7, 6));
+	printf("%d\n", ft_iterative_power(-5, 8));
+	printf("%d\n", ft_iterative_power(-1, 10));
 	return (0);
 }
