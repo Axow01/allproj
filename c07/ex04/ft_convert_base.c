1c777d9459024cd029c0f1e914c09ef50491edb1
Mickael Marcotte <mmarcott@c1r5p1.42quebec.com>
2022-10-03 12:24:55 -0400

    jspu

--- c07/ex04/ft_convert_base.c
+++ c07/ex04/ft_convert_base.c
@@ -1,40 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 11:33:59 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/10/02 22:51:07 by mmarcott         ###   ########.fr       */
+/*   Created: 2022/10/02 21:38:03 by mmarcott          #+#    #+#             */
+/*   Updated: 2022/10/02 21:38:04 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_prime(int nb)
-{
-	int	rem;
-
-	rem = nb - 1;
-	if (nb == 1 || nb == 0)
-		return (0);
-	while (rem > 1)
-	{
-		if (nb % rem == 0)
-			return (0);
-		rem--;
-	}
-	return (1);
-}
-
-int	ft_find_next_prime(int nb)
-{	
-	if (nb < 0)
-		return (2);
-	if (nb >= 2147483643)
-		return (2147483647);
-	if (ft_is_prime(nb))
-		return (nb);
-	while (!ft_is_prime(nb))
-		nb++;
-	return (nb);
-}
